66d5c2080fe718827316313e91d16ff30ad97db3
felixhertrampf <hertrampf.f@gmail.com>
2019-09-10 10:35:25 +0200

    Update PyOptional.py

--- PyOptional.py
+++ PyOptional.py
@@ -1,16 +1,17 @@
-from typing import Any, Callable, TypeVar, Type
+from typing import Any, Callable, TypeVar, Type, Optional
 
 
-class PyOptional:
+class PyOptional(Optional):
 	T: TypeVar = TypeVar("T")
+	Optional = Callable[[T], Any]
 
 	def __init__(self, obj: T):
 		self.value: Any = obj
 
-	def get(self) -> T:
+	def get(self) -> Optional[T]:
 		return self.value
 
-	def or_else(self, default) -> T:
+	def or_else(self, default: T) -> T:
 		return self.value or default
 
 	def or_else_do(self, _lambda: Callable) -> Any:
@@ -28,7 +29,7 @@ class PyOptional:
 	def is_present(self) -> bool:
 		return self.value is not None
 
-	def if_present(self, _lambda: Callable, default) -> Any:
+	def if_present(self, _lambda: Callable, default: T) -> Any:
 		if self.is_present():
 			return _lambda(self.value)
 		else:
