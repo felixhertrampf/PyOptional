cdad3a39d70001a56cde40d70a81629677e1f396
felixhertrampf <hertrampf.f@gmail.com>
2019-09-12 10:17:48 +0200

    PyOptional now knows it's type
    PyOptional can now be used as
    optional: PyOptional[str] = ...
    
    The IDE now knows what kind of object is wrapped. 
    
    Implemented with help by michael0x2a https://stackoverflow.com/users/646543/michael0x2a

--- PyOptional.py
+++ PyOptional.py
@@ -1,12 +1,16 @@
-from typing import Any, Callable, TypeVar, Type, Optional
+from typing import Any, Callable, TypeVar, Type, Optional, Generic
+
+T = TypeVar('T')
 
 
-class PyOptional(Optional):
-	T: TypeVar = TypeVar("T")
-	Optional = Callable[[T], Any]
+class PyOptional(Generic[T]):
 
-	def __init__(self, obj: T):
-		self.value: Any = obj
+	@classmethod
+	def empty(cls):
+		return PyOptional(None)
+
+	def __init__(self, obj: Optional[T]) -> None:
+		self.value = obj
 
 	def get(self) -> Optional[T]:
 		return self.value
@@ -14,20 +18,26 @@ class PyOptional(Optional):
 	def or_else(self, default: T) -> T:
 		return self.value or default
 
-	def or_else_do(self, _lambda: Callable) -> Any:
-		if self.is_present():
+	def or_else_do(self, _lambda: Callable) -> T:
+		if self.value:
 			return self.value
 		else:
 			return _lambda()
 
 	def or_raise(self, exception: Type[Exception]) -> T:
-		if self.is_present():
+		if self.value:
 			return self.value
 		else:
 			raise exception
 
 	def is_present(self) -> bool:
-		return self.value is not None
+		if self.value is not None:
+			return True
+
+		return False
+
+	def is_absent(self) -> bool:
+		return self.value is None
 
 	def if_present(self, _lambda: Callable, default: T) -> Any:
 		if self.is_present():
