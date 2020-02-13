bef416113089abebd0c0deac478408036b357bd1
felixhertrampf <hertrampf.f@gmail.com>
2020-02-13 13:32:30 +0100

    Changed exception type to Exception from Type[Exception]

--- PyOptional.py
+++ PyOptional.py
@@ -1,52 +1,50 @@
-from typing import Any, Callable, TypeVar, Type, Optional, Generic
+from typing import Callable, TypeVar, Type, Optional, Generic
 
 T = TypeVar('T')
 
 
 class PyOptional(Generic[T]):
+    value: T
 
-	@classmethod
-	def empty(cls):
-		return PyOptional(None)
+    @classmethod
+    def empty(cls):
+        return PyOptional[T](None)
 
-	def __init__(self, obj: Optional[T]) -> None:
-		self.value = obj
+    def __init__(self, value: Optional[T]) -> None:
+        self.value = value
 
-	def get(self) -> Optional[T]:
-		return self.value
+    def get(self) -> Optional[T]:
+        return self.value
 
-	def or_else(self, default: T) -> T:
-		return self.value or default
+    def or_else(self, default: T) -> T:
+        return self.value or default
 
-	def or_else_do(self, _lambda: Callable) -> T:
-		if self.value:
-			return self.value
-		else:
-			return _lambda()
+    def or_else_do(self, _lambda: Callable) -> T:
+        if self.value:
+            return self.value
+        else:
+            return _lambda()
 
-	def or_raise(self, exception: Type[Exception]) -> T:
-		if self.value:
-			return self.value
-		else:
-			raise exception
+    def or_raise(self, exception: Exception) -> T:
+        if self.value:
+            return self.value
+        else:
+            raise exception
 
-	def is_present(self) -> bool:
-		if self.value is not None:
-			return True
+    def is_present(self) -> bool:
+        return self.value is not None
 
-		return False
+    def is_none(self) -> bool:
+        return self.value is None
 
-	def is_absent(self) -> bool:
-		return self.value is None
+    def if_present(self, _lambda: Callable, default: T) -> T:
+        if self.is_present():
+            return _lambda(self.value)
+        else:
+            return default
 
-	def if_present(self, _lambda: Callable, default: T) -> Any:
-		if self.is_present():
-			return _lambda(self.value)
-		else:
-			return default
-
-	def if_present_or_raise(self, _lambda: Callable, exception: Type[Exception]) -> Any:
-		if self.is_present():
-			return _lambda(self.value)
-		else:
-			raise exception
+    def if_present_or_raise(self, _lambda: Callable, exception: Exception) -> T:
+        if self.is_present():
+            return _lambda(self.value)
+        else:
+            raise exception
